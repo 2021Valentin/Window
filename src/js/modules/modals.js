f95ce3493f3a10e0f1614f7ec6ec7d2b3d6470c2
Valentin <valentin.mudriak@gmail.com>
2023-05-02 09:10:01 +0300

    added modals

---
+++ src/js/modules/modals.js
@@ -0,0 +1,41 @@
+const modals = () => {
+    function bindModal(triggerSelector, modalSelector, closeSelector) {
+        const trigger = document.querySelectorAll(triggerSelector),
+              modal = document.querySelector(modalSelector),
+              close = document.querySelector(closeSelector);
+
+        trigger.forEach(item => {
+            item.addEventListener('click', (e) => {
+            if (e.target) {
+                e.preventDefault();
+            }
+            modal.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+            // document.body.classList.add('modal-open');
+            });
+        });
+        close.addEventListener('click', (e) => {
+            modal.style.display = 'none';
+            document.body.style.overflow = '';
+            // document.body.classList.remove('modal-open');
+        });
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal) {
+                modal.style.display = 'none';
+                document.body.style.overflow = '';
+                // document.body.classList.remove('modal-open');
+            }
+        });
+    }
+    function showModalByTime(modalSelector, time) {
+        setTimeout(function () {
+            document.querySelector(modalSelector).style.display = 'block';
+            document.querySelector(modalSelector).style.overflow = 'hidden';
+        }, time);
+    }
+    bindModal('.popup_engineer_btn', '.popup', '.popup .popup_close');
+    bindModal('.phone_link', '.popup', '.popup .popup_close');
+    showModalByTime('.popup', 3000);
+};
+
+export default modals;
